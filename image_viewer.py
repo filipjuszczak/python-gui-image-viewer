43cf6b15417e295ed47a71fb0664070fed24969f
kaszczuj <juszczak.filip24@gmail.com>
2021-06-11 14:10:23 +0200

    changed image file names to their original names

--- image_viewer.py
+++ image_viewer.py
@@ -18,31 +18,33 @@ from PIL import ImageTk, Image
 root = Tk()
 # Setting the window title.
 root.title('Image Viewer')
+# Don't allow the window to be resized.
+root.resizable(False, False)
 
 # Creating image.
-my_img1 = Image.open('images/1.jpg')
+my_img1 = Image.open('images/pexels-olya-prutskova-7433162.jpg')
 # Resizing the image.
 my_img1 = my_img1.resize((400, 600), Image.ANTIALIAS)
 # Creating a photoimage to use in the program.
 my_img1 = ImageTk.PhotoImage(my_img1)
 
-my_img2 = Image.open('images/2.jpg')
+my_img2 = Image.open('images/pexels-damian-barczak-8148690.jpg')
 my_img2 = my_img2.resize((400, 600), Image.ANTIALIAS)
 my_img2 = ImageTk.PhotoImage(my_img2)
 
-my_img3 = Image.open('images/3.jpg')
+my_img3 = Image.open("images/pexels-elijah-o'donnell-4335608.jpg")
 my_img3 = my_img3.resize((400, 600), Image.ANTIALIAS)
 my_img3 = ImageTk.PhotoImage(my_img3)
 
-my_img4 = Image.open('images/4.jpg')
+my_img4 = Image.open('images/pexels-gantas-vaiÄiulÄnas-7159608.jpg')
 my_img4 = my_img4.resize((400, 600), Image.ANTIALIAS)
 my_img4 = ImageTk.PhotoImage(my_img4)
 
-my_img5 = Image.open('images/5.jpg')
+my_img5 = Image.open('images/pexels-lelani-badenhorst-6790685.jpg')
 my_img5 = my_img5.resize((400, 600), Image.ANTIALIAS)
 my_img5 = ImageTk.PhotoImage(my_img5)
 
-my_img6 = Image.open('images/6.jpg')
+my_img6 = Image.open('images/pexels-maria-camila-castaÃ±o-7332786.jpg')
 my_img6 = my_img6.resize((400, 600), Image.ANTIALIAS)
 my_img6 = ImageTk.PhotoImage(my_img6)
 
@@ -58,10 +60,22 @@ image_list = [
     my_img5,
     my_img6
 ]
+# List of image file names.
+image_names = [
+    'pexels-olya-prutskova-7433162.jpg',
+    'pexels-damian-barczak-8148690.jpg',
+    "pexels-elijah-o'donnell-4335608.jpg",
+    'pexels-gantas-vaiÄiulÄnas-7159608.jpg',
+    'pexels-lelani-badenhorst-6790685.jpg',
+    'pexels-maria-camila-castaÃ±o-7332786.jpg'
+]
 
 # Creating a label which will display images.
-my_label = Label(image=my_img1)
-my_label.grid(row=0, column=0, columnspan=3)
+image_label = Label(image=my_img1)
+# Creating a status label displaying the image number.
+status = Label(root, text='Image 1 of {0}'.format(len(image_list)), bd=1)
+# Creating a label displaying the name of the image file.
+image_name = Label(root, text=image_names[0])
 
 # A global variable representing an index of the image_list image.
 index = 0
@@ -72,8 +86,10 @@ def change_image(change):
     Displays images back and forth.
     :param change: char value ('b' -> backwards or 'f' -> forward)
     """
-    global my_label
+    global image_label
     global index
+    global status
+    global image_name
 
     """
     If we're going backwards and we don't go below the lowest index in the
@@ -81,16 +97,31 @@ def change_image(change):
     """
     if change == 'b' and index - 1 >= 0:
         # Remove the old image.
-        my_label.grid_forget()
+        image_label.grid_forget()
         # Decrease index by 1.
         index -= 1
         """
         Re-create the label displaying images and display the previous 
         image in the list.
         """
-        my_label = Label(image=image_list[index])
+        image_label = Label(image=image_list[index])
         # Display the label on the window.
-        my_label.grid(row=0, column=0, columnspan=3)
+        image_label.grid(row=0, column=0, columnspan=3)
+        # Re-creating the status label and setting the correct value.
+        status = Label(root,
+                       text='Image {0} of {1}'.format(index + 1,
+                                                      len(image_list)),
+                       bd=1
+                       )
+        # Display the label on the window.
+        status.grid(row=3, column=0, columnspan=3, sticky=W + E)
+        # Deleting the old image file name label.
+        image_name.destroy()
+        # Re-creating the image file name label and setting the correct value.
+        image_name = Label(root, text=image_names[index])
+        # Display the label on the window.
+        image_name.grid(row=1, column=0, columnspan=3)
+
 
     """
     If we're going forwards and we don't go above the highest index in the 
@@ -98,16 +129,30 @@ def change_image(change):
     """
     if change == 'f' and index + 1 < len(image_list):
         # Remove the old image.
-        my_label.grid_forget()
+        image_label.grid_forget()
         # Increase index by 1.
         index += 1
         """
         Re-create the label displaying images and display the next
         image in the list.
         """
-        my_label = Label(image=image_list[index])
+        image_label = Label(image=image_list[index])
         # Display the label on the window.
-        my_label.grid(row=0, column=0, columnspan=3)
+        image_label.grid(row=0, column=0, columnspan=3)
+        # Re-creating the status label and setting the correct value.
+        status = Label(root,
+                       text='Image {0} of {1}'.format(index + 1,
+                                                      len(image_list)),
+                       bd=1
+                       )
+        # Display the label on the window.
+        status.grid(row=3, column=0, columnspan=3, sticky=W + E)
+        # Deleting the old image file name label.
+        image_name.destroy()
+        # Re-creating the image file name label and setting the correct value.
+        image_name = Label(root, text=image_names[index])
+        # Display the label on the window.
+        image_name.grid(row=1, column=0, columnspan=3)
 
 
 # Creating buttons that will let user view images.
@@ -117,10 +162,13 @@ button_exit = Button(root, text='EXIT', padx=20, pady=20, command=root.quit)
 button_forward = Button(root, text='>>', padx=20, pady=20,
                         command=lambda: change_image('f'))
 
-# Putting buttons into the window.
-button_backward.grid(row=1, column=0)
-button_exit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
+# Displaying all elements.
+image_label.grid(row=0, column=0, columnspan=3)
+image_name.grid(row=1, column=0, columnspan=3)
+button_backward.grid(row=2, column=0)
+button_exit.grid(row=2, column=1)
+button_forward.grid(row=2, column=2, pady=10)
+status.grid(row=3, column=0, columnspan=3, sticky=W + E)
 
 # The main program loop.
 root.mainloop()
